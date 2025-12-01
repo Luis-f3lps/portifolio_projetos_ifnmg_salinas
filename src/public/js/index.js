49ba5959f55eade97f29751069ea4b0d2ad2bb19
Luis Gustavo B. Santiago <74880596+Luis-f3lps@users.noreply.github.com>
2025-12-01 16:14:33 -0300

    Update index.js

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -712,7 +712,7 @@ function criarGraficoPizzaTipos(data) {
             backgroundColor: PALETA_CORES_TIPOS,
             borderColor: "#fff",
             borderWidth: 2,
-            radius: 200,  // <--- VALOR FIXO (Sem aspas, sem %)
+            radius: 150,  // <--- VALOR FIXO (Sem aspas, sem %)
             cutout: "80%", // O buraco será 60% desse raio fixo
           },
         ],
@@ -793,7 +793,7 @@ function criarGraficoStatusProdutos(data) {
             backgroundColor: ["#2ecc71", "#e74c3c"],
             borderColor: "#fff",
             borderWidth: 2,
-            radius: 200, // <--- O MESMO VALOR FIXO AQUI
+            radius: 150, // <--- O MESMO VALOR FIXO AQUI
             cutout: "80%",
           },
         ],
